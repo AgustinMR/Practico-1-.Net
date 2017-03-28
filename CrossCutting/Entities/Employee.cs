5e4a4d13f99b371bebbff577dc65174b172eccd3
Agustin Maidana Rodriguez <agustin.maidana@fing.edu.uy>
2017-03-28 17:41:26 -0300

    Primer commit

---
+++ CrossCutting/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Entities {
+
+    public abstract class Employee {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private DateTime StartDate { get; set; }
+    }
+}
