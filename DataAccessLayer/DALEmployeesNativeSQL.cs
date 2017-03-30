bd36b894f6ccfdc0f3e367d2786c14ff9edd0aba
Pedro Omar Moretto Rodriguez <pedro.moretto@fing.edu.uy>
2017-03-30 01:02:21 -0300

    dia uno... creo que esto se va a subir... use git branch para ver donlo va a subir ... y veo que es mi rama... tengo mis dudas... alguien se comio al pequeño timy.... vienen por mi ... lo se

--- DataAccessLayer/DALEmployeesNativeSQL.cs
+++ DataAccessLayer/DALEmployeesNativeSQL.cs
@@ -4,9 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
-namespace DataAccessLayer
-{
+
+namespace DataAccessLayer{
+   public class conexion{
+        SqlConnection tcp;
+        SqlCommand cmd;
+        SqlDataReader cin;
+
+        public conexion(){
+
+            try{
+                tcp = new SqlConnection("Data Source =.; Initial Catalog =.NET; Integrated Security = True");
+                tcp.Open();
+                MessageBox.Show("Conextado");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectarse" + ex.ToString());
+
+            }
+        }
+    }
+
+
+
     public class DALEmployeesNativeSQL : IDALEmployees
     {
         public void AddEmployee(Employee emp)
--- PresentationLayerWinform/Program.cs
+++ PresentationLayerWinform/Program.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace PresentationLayerWinform
-{
-    static class Program
-    {
+
+namespace PresentationLayerWinform {
+    static class Program{
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new EmployeeList());
+        //[STAThread]
+        static void Main(string[] args){
+           
+           // MessageBox.Show("Hola mundo");
+           /// Application.EnableVisualStyles();
+           // Application.SetCompatibleTextRenderingDefault(false);
+           // Application.Run(new EmployeeList());
         }
     }
 }
