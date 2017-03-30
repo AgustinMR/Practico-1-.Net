8f286d3393cfab850fd4e7746fb2dd4034e66581
Agustin Maidana Rodriguez <agustin.maidana@fing.edu.uy>
2017-03-30 19:07:07 -0300

    Todo el trabajo de pedro

--- DataAccessLayer/DALEmployeesNativeSQL.cs
+++ DataAccessLayer/DALEmployeesNativeSQL.cs
@@ -4,9 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace DataAccessLayer
 {
+
+    class conexion
+    {
+        SqlConnection tcp;
+        SqlCommand cmd;
+        SqlDataReader cin;
+
+        public conexion()
+        {
+
+            try
+            {
+                tcp = new SqlConnection("Data Source =.; Initial Catalog =.NET; Integrated Security = True");
+                tcp.Open();
+                MessageBox.Show("Conextado");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al conectarse" + ex.ToString());
+
+            }
+        }
+    }
+
     public class DALEmployeesNativeSQL : IDALEmployees
     {
         public void AddEmployee(Employee emp)
